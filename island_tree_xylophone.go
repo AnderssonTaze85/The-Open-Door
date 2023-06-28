bc9c7a396bc9a57f87772f11a1323f8db947e2dc
AnderssonTaze85 <137460501+AnderssonTaze85@users.noreply.github.com>
2023-06-29 01:20:48 +0700

    Improve data filtering

---
+++ island_tree_xylophone.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Opening the door
+	fmt.Println("You slowly creak open the door...")
+	
+	// Step in to the unknown
+	fmt.Println("You step in to the unknown...")
+	
+	// Take a look around
+	fmt.Println("You take a look around the room...")
+	
+	// Hear a strange noise
+	fmt.Println("You hear a strange noise coming from down the hall...")
+	
+	// Follow the noise
+	fmt.Println("You cautiously follow the noise down the hall...")
+	
+	// See a strange figure
+	fmt.Println("You suddenly see a strange figure in the doorway...")
+	
+	// Move closer
+	fmt.Println("You slowly move closer, not sure of what to expect...")
+	
+	// Find out who/what it is
+	fmt.Println("You try to make out who/what the figure is...")
+	
+	// Realize it's a ghost
+	fmt.Println("Suddenly you realize it's a ghost and start to turn around...")
+	
+	// Hear the ghost speaking
+	fmt.Println("But before you can leave, you hear the ghost speaking...")
+	
+	// Ghost speaks
+	fmt.Println("'Welcome to my home, young one. Please, stay a while...'")
+	
+	// Make a decision
+	fmt.Println("You must decide whether to stay or leave...")
+	
+	// Stay and talk
+	fmt.Println("You decide to stay and talk to the ghost...")
+	
+	// Listen to the story
+	fmt.Println("You listen to the ghost's story with amazement...")
+	
+	// Learn the ghost's secrets
+	fmt.Println("You learn the ghost's secrets with every passing minute...")
+	
+	// Talk for hours
+	fmt.Println("You talk with the ghost for what seems like hours...")
+	
+	// Ghost leaves
+	fmt.Println("Suddenly the ghost disappears, leaving you with a newfound knowledge...")
+	
+	// Think about the experience
+	fmt.Println("You think about the experience you just had and what you learned...")
+	
+	// Go back through the door
+	fmt.Println("You go back through the door and out into the world...")
+	
+	// Take the knowledge with you
+	fmt.Println("You take the knowledge you just gained with you and move forward with your life...")
+}
